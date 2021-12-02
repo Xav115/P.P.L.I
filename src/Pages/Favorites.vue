a3449271cbceda270115074a19971c294b57fe38
Xav115 <xavier1386a@gmail.com>
2021-12-02 13:05:49 -0400

    Change back button

--- src/Pages/Favorites.vue
+++ src/Pages/Favorites.vue
@@ -1,5 +1,10 @@
 <template>
-  <base-layout page-title="Favoritos" page-default-back-link="/Home">
+  <base-layout page-title="Favoritos">
+    <template v-slot:back>
+      <ion-button href="/Home" fill="clear">
+        back
+      </ion-button>
+    </template>
     <template v-slot:invisible>
       <ion-button disabled>
         <ion-icon slot="icon-only" :icon="information"></ion-icon>
